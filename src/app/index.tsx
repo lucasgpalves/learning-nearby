87d2edcaad4b42ace126391bd04fa38b2ea4be7f
Lucas Alves <alvez.lucas2702@gmail.com>
2024-12-11 19:08:21 -0300

    aula 2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,9 @@
+import { View } from "react-native"
+import { router } from "expo-router"
+
 import { Welcome } from "@/components/welcome"
 import { Steps } from "@/components/steps"
-import { View, Text } from "react-native"
+import { Button } from "@/components/button"
 
  export default function Index() {
     return (
@@ -9,6 +12,9 @@ import { View, Text } from "react-native"
         }}>
             <Welcome />
             <Steps />
+            <Button onPress={() => router.navigate("/home")}>
+                <Button.Title>Começar</Button.Title>
+            </Button>
         </View>
     )
  }
